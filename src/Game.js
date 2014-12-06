a6589b44460618e0149566ab2c815d20167bb1a4
MChay <napolo.1988@gmail.com>
2014-12-06 17:03:47 -0600

    Inicio de tutorial de juego de html 5.
    Se han agregado los archivos.

---
+++ src/Game.js
@@ -0,0 +1,18 @@
+Ball.Game = function (game) {
+    keys = null;
+    ball = null;
+    walls = null;
+    timer = 0;
+    totalTimer = 0;
+    loop = null;
+    firstRun = true;
+    level = 0;
+    sfx_bounce = null;
+    maxLevels = 5;
+    audio = true;
+};
+Ball.Game.prototype = {
+    create: function () {
+        
+    }
+};
