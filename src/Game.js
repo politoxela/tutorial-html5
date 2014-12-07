3b0419a1ac02da7b2cfc829ca6ac8f14afebc577
MChay <napolo.1988@gmail.com>
2014-12-06 18:39:46 -0600

    Agregada la bola a la pantalla pricipal de tuorial.

--- src/Game.js
+++ src/Game.js
@@ -13,6 +13,27 @@ Ball.Game = function (game) {
 };
 Ball.Game.prototype = {
     create: function () {
+        ball = this.add.sprite((320-22)/2,450,'ball');
+        ball.anchor.setTo(0.5,0.5);
+        ball.body.bounce.setTo(0.3,0.3);
+        ball.body.setCircle(10, 11, 11);
+        ball.body.linearDamping = 1;
         
+        keys = this.game.input.keyboard.createCursorKeys();
+        
+        var force = 10;
+        
+        if(keys.left.isDown){
+            ball.body.velocity.x -= force;
+        }
+        else if(keys.right.isDown){
+            ball.body.velocity.x += force;
+        }
+        if(keys.up.isDown){
+            ball.body.velocity.y -= force;
+        }
+        else if(keys.down.isDown){
+            ball.body.velocity.y += force;
+        }
     }
 };
